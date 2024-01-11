9fa21c255a317e0d209b3326ee898c28701b388f
Bruce Rebello <bruce.rebello@gmail.com>
2024-01-11 13:47:30 -0500

    new layout set

--- ui/src/components/game/index.tsx
+++ ui/src/components/game/index.tsx
@@ -34,81 +34,79 @@ export default function Game() {
         <h1 class="text-3xl font-bold">Commonly Odd</h1>
       </div>
 
-      {/* Main Section */}
-      <div class="flex flex-row bg-slate-50">
-        <div class="flex w-1/5 justify-center items-center">
-          <StartSession />
-        </div>
-        <div class="flex w-3/5 flex-row items-center justify-center">
-          <CopyLink />
-          <input
-            type="text"
-            class="border p-2 rounded ml-2 w-full md:w-auto lg:w-5/12"
-            readOnly
-            value={sessionLink()}
-          />
-        </div>
-        <div class="flex w-1/5 justify-center items-center">
-          <div class="text-center">
-            <p class="text-lg">Timer</p>
-            <p class="text-lg font-bold">{timer()}</p>
-          </div>
-        </div>
-      </div>
-
       {/* Bottom Section */}
       <div class="flex flex-grow">
-        <div class="flex flex-col items-center w-2/12 pt-20 lg:pt-6 sm:justify-start lg:justify-center bg-slate-50">
-          <div class="flex flex-col space-y-20 items-center">
-            <Button sx={{ bgcolor: "#fecdd3", color: "#db2777" }}>
-              <InfoModal
-                title={gameRules.title}
-                content={gameRules.content}
-                icon={<RuleOutlined fontSize="large" />}
-                openModal={showRulesModal()}
-                setOpenModal={setShowRulesModal}
-              />
-            </Button>
-            <Button sx={{ bgcolor: "#fecdd3", color: "#db2777" }}>
-              <InfoModal
-                title={faqTerminology.title}
-                content={faqTerminology.content}
-                icon={<SportsEsportsOutlined fontSize="large" />}
-                openModal={showFAQModal()}
-                setOpenModal={setShowFAQModal}
-              />
-            </Button>
+        <div class="flex flex-col w-2/12 justify-start bg-slate-50">
+          <div class="flex flex-col items-center justify-start space-y-20 ">
+            <Button>Start Game</Button>
+          </div>
+          <div class="flex flex-col space-y-20 items-center justify-center pt-44">
+            <InfoModal
+              title={gameRules.title}
+              content={gameRules.content}
+              icon={<RuleOutlined fontSize="large" />}
+              openModal={showRulesModal()}
+              setOpenModal={setShowRulesModal}
+            />
+            <InfoModal
+              title={faqTerminology.title}
+              content={faqTerminology.content}
+              icon={<SportsEsportsOutlined fontSize="large" />}
+              openModal={showFAQModal()}
+              setOpenModal={setShowFAQModal}
+            />
             <Button sx={{ bgcolor: "#fecdd3", color: "#db2777" }}>
               <SearchOutlined fontSize="large" />
             </Button>
           </div>
         </div>
-        <div class="flex w-11/12 flex-col lg:flex-row items-center lg:space-x-4 bg-slate-100">
-          <div class="py-4">
-            <p class="text-center">Obj1</p>
-            <img src="https://via.placeholder.com/400" alt="Obj1" />
+        {/* flex-col lg:flex-row items-center lg:space-x-4 */}
+        <div class="flex w-11/12 flex-col bg-slate-100 ">
+          <div class="flex flex-row items-center justify-center bg-slate-50   ">
+            <CopyLink />
+            <input
+              type="text"
+              class="border p-2 rounded ml-2 w-full md:w-auto lg:w-5/12"
+              readOnly
+              value={sessionLink()}
+            />
           </div>
-          <div class="py-4">
-            <p class="text-center">Obj2</p>
-            <img src="https://via.placeholder.com/400" alt="Obj2" />
+
+          <div class="flex flex-row items-center justify-center  pt-12   ">
+            <StartSession />
           </div>
-          <div class="py-4">
-            <p class="text-center">Obj3</p>
-            <img src="https://via.placeholder.com/400" alt="Obj3" />
+
+          <div class="flex flex-col lg:flex-row justify-center items-center lg:space-x-4 lg:pt-20  ">
+            <div class="py-4">
+              <p class="text-center">Obj1</p>
+              <img src="https://via.placeholder.com/400" alt="Obj1" />
+            </div>
+            <div class="py-4">
+              <p class="text-center">Obj2</p>
+              <img src="https://via.placeholder.com/400" alt="Obj2" />
+            </div>
+            <div class="py-4">
+              <p class="text-center">Obj3</p>
+              <img src="https://via.placeholder.com/400" alt="Obj3" />
+            </div>
           </div>
         </div>
-        <div class="flex flex-col items-center w-2/12 pt-20 lg:pt-6 sm:justify-start lg:justify-center bg-slate-50">
-          <div class="flex flex-col space-y-20 items-center">
+        <div class="flex flex-col w-2/12 justify-start bg-slate-50">
+          <div class="flex flex-col items-center justify-start space-y-20 ">
+            <div class="text-center">
+              <p class="text-lg">Timer</p>
+              <p class="text-lg font-bold">{timer()}</p>
+            </div>
+          </div>
+          <div class="flex flex-col space-y-20 items-center justify-center pt-40">
             <EndGameSession />
-            <Button sx={{ bgcolor: "#fecdd3", color: "#db2777" }}>
-              <InfoModal
-                title={legalDocuments.title}
-                content={legalDocuments.content}
-                icon={<PrivacyTipOutlined fontSize="large" />}
-                openModal={showLegalModal()}
-                setOpenModal={setShowLegalModal}
-              />
-            </Button>
+            <InfoModal
+              title={legalDocuments.title}
+              content={legalDocuments.content}
+              icon={<PrivacyTipOutlined fontSize="large" />}
+              openModal={showLegalModal()}
+              setOpenModal={setShowLegalModal}
+            />
             <Button sx={{ bgcolor: "#fecdd3", color: "#db2777" }}>
               <SportsScoreOutlined fontSize="large" />
             </Button>
