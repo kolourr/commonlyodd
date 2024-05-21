b89e95cbfa718ed233af3f4954d9f7aa784146ac
Bruce <contact@estimatebodyfat.com>
2024-05-21 19:44:01 -0400

    updated video

--- ui/src/components/auth_payments_landing/landing.tsx
+++ ui/src/components/auth_payments_landing/landing.tsx
@@ -456,7 +456,7 @@ const LandingPage: Component = () => {
         <div class="w-full mb-4 text-base md:text-lg">
           <video controls class="w-full h-auto shadow-lg">
             <source
-              src="https://media.commonlyodd.com/how_it_works_update.mp4"
+              src="https://media.commonlyodd.com/how_it_works_update_21.mp4"
               type="video/mp4"
             />
             Your browser does not support the video tag.
