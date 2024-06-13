f9c52588be9cb3c3c18e3bda8758ec8d834be13c
Bruce <contact@estimatebodyfat.com>
2024-06-13 15:00:07 -0400

    set countdown to 10 to 20 seconds

--- ui/src/components/game/start_session/countdown.tsx
+++ ui/src/components/game/start_session/countdown.tsx
@@ -51,7 +51,7 @@ export default function Countdown({ setCountdown }: CountdownProps) {
             color: "#f9fafb",
           }}
         >
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((score) => (
+          {[10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map((score) => (
             <MenuItem
               class="bg-gradient-to-r from-slate-900 via-zinc-950   to-slate-900"
               value={score}
